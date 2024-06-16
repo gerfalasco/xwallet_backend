b64eb7c5d85558f41b78a888522ffbb15231dba8
Andy-Godoy <andres.godoyparise@gmail.com>
2024-06-16 12:29:27 -0300

    Feat(various): Various changes.
    - Changed certificate file
    - Configured app to pull new file

--- src/main/java/com/xwallet/xwallet/XwalletApplication.java
+++ src/main/java/com/xwallet/xwallet/XwalletApplication.java
@@ -2,14 +2,33 @@ package com.xwallet.xwallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.io.InputStream;
+import java.security.KeyStore;
 
 @SpringBootApplication
 @EnableScheduling
 public class XwalletApplication {
 
 	public static void main(String[] args) {
+		cargarPfx();
 		SpringApplication.run(XwalletApplication.class, args);
+	}
+
+	private static void cargarPfx() {
+		try {
+			// Cargar el archivo PFX desde resources
+			InputStream pfxInputStream = new ClassPathResource("certificate.pfx").getInputStream();
+
+			// Crear un objeto KeyStore y cargar el archivo PFX
+			KeyStore pfxKeyStore = KeyStore.getInstance("PKCS12");
+			pfxKeyStore.load(pfxInputStream, "".toCharArray());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/main/java/com/xwallet/xwallet/utils/CorsConfig.java
+++ src/main/java/com/xwallet/xwallet/utils/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.xwallet.xwallet.utils;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,16 +9,34 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+    private final String mappings;
+    private final String allowedOrigins;
+    private final String allowedMethods;
+    private final String allowedHeaders;
+    private final String allowCredentials;
+
+    public CorsConfig(@Value("${spring.web.cors.mappings}") String mappings,
+                      @Value("${spring.web.cors.allowed-origins}") String allowedOrigins,
+                      @Value("${spring.web.cors.allowed-methods}") String allowedMethods,
+                      @Value("${spring.web.cors.allowed-headers}") String allowedHeaders,
+                      @Value("${spring.web.cors.allow-credentials}") String allowCredentials) {
+        this.mappings = mappings;
+        this.allowedOrigins = allowedOrigins;
+        this.allowedMethods = allowedMethods;
+        this.allowedHeaders = allowedHeaders;
+        this.allowCredentials = allowCredentials;
+    }
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOriginPatterns("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                registry.addMapping(mappings)
+                        .allowedOriginPatterns(allowedOrigins)
+                        .allowedMethods(allowedMethods)
+                        .allowedHeaders(allowedHeaders)
+                        .allowCredentials(Boolean.parseBoolean(allowCredentials));
             }
         };
     }
